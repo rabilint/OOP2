ba9f9fea54216ccb9e37bde64e1aabaa43d9c972
rabilint <Nikita.Maiboroda@lnu.edu.ua>
2025-03-20 17:56:15 +0200

    Change after lector QA

--- AIP.cpp
+++ AIP.cpp
@@ -31,11 +31,11 @@ void checkPointInTriangle(Point a, Point b, Point c, Point point) { //перев
     bool allNegative = (cross1 < 0 && cross2 < 0 && cross3 < 0);
     if (totalArea < 1e-6) {std::cout << "Трикутник не вироджений!\n";}
     else if (allPositive || allNegative)
-        std::cout << "Точка належить трикутнику.";
+        std::cout << "Точка x:" << point.x << " y:" << point.y <<" — належить трикутнику.\n";
 
     else if (cross1 == 0 || cross2 == 0 || cross3 == 0)
-        std::cout << "Точка лежить на межі трикутника.";
-    else std::cout << "Точка поза трикутником.";
+        std::cout << "Точка x:" << point.x << " y:" << point.y <<" — лежить на межі трикутника.\n";
+    else std::cout << "Точка x:" << point.x << " y:" << point.y <<" — не належить трикутнику.\n";
 };
 
 double laysOnBorder(Point a, Point b, Point point) {//випадків, коли точка лежить на межі трикутника.
@@ -44,11 +44,11 @@ double laysOnBorder(Point a, Point b, Point point) {//випадків, коли
 
 double contain(Point a, Point b, Point c, Point point) { //чи належить точка до трикутника
     double total_area = area(a, b, c);
-    if (laysOnBorder(a,b,point) || laysOnBorder(b,c,point) || laysOnBorder(c,a,point)) {std::cout << "Точка лежить на границі трикутника\n"; return 0; }
+    if (laysOnBorder(a,b,point) || laysOnBorder(b,c,point) || laysOnBorder(c,a,point)) {std::cout << "Точка х:" << point.x << " у:" << point.y << " — лежить на границі трикутника\n"; return 0; }
     else {
         if (total_area < 1e-6) {std::cout << "Трикутник не вироджений!\n";  return false;}
         double tmp = (area(point, b, c) + area(a, point, c) + area(a, b, point)) / total_area;
-        std::cout << (fabs(tmp - 1.0) < 1e-6 ? "Точка належить трикутнику." : "Точка не належить трикутнику.") << std::endl;
+        (fabs(tmp - 1.0) < 1e-6 ? std::cout << "Точка: х:" << point.x << " y:" << point.y << " — належить трикутнику." : std::cout << "Точка: х:" << point.x << " у:" << point.y << " — не належить трикутнику") << std::endl;
         return 0;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 #include "AIP.h"
+#include <vector>
 
 int main() {
-    // double points[];
-    Point point, a, b, c;
+    std::vector<Point> v1;
+    Point a, b, c;
+    bool end = false;
     std::cout << "Введіть вершини трикутників: \nКоординати першої вершини: ";
     std::cin >> a.x >> a.y;
     std::cout << "\nКоординати другої вершини: "; std::cin>> b.x >> b.y;
     std::cout << "\nКоординати третьої вершини: "; std::cin >> c.x >> c.y;
 
-    std::cout << "Введіть х та у точки: ";
-    std::cin >> point.x >> point.y;
-    contain(a, b, c, point);
+    do {
+        Point point;
+        std::cout << "Введіть х та у точки: ";
+        std::cin >> point.x >> point.y;
+        v1.push_back(point);
+        std::cout << "end? >0 - yes; <0 - no" << std::endl;
+        std::cin >> end;
+    } while (!end);
 
+    if (v1.empty()) { //перевірка чи ввів щось користувач.
+        std::cout << "В масиві точки не було знайдено!";
+        return 0;
+    }
+
+    for (int i = 0; i < v1.size(); i++) {
+        contain(a, b, c, v1.at(i));
+    }
     std::cout <<"Перевірка альтернативним меодом (методом векторного добутку): \n" << std::endl;
-    checkPointInTriangle(a, b, c, point);
+    for (int i = 0; i < v1.size(); i++) {
+        checkPointInTriangle(a, b, c, v1.at(i));
+    }
+
 
 
     return 0;
